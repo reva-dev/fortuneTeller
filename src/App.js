fadfd33748bee1c053ccc98e3bc9cb0fba7f40de
Reva Arora <revaarora.1991@gmail.com>
2020-06-21 01:51:44 +0530

    css fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import fortunelist from './fortunelist.json';
-import './App.css';
+import React from "react";
+import fortunelist from "./fortunelist.json";
+import "./App.css";
+// import './mysass.scss';
 
 function App() {
- 
- const today = fortunelist[Math.floor( Math.random() * (356) )];
+  const today = fortunelist[Math.floor(Math.random() * 356)];
   return (
-    <div className="App">
-      {today}
+    <div className='App'>
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <div id='title'>
+        <span>{today}</span>
+      </div>
     </div>
   );
 }
